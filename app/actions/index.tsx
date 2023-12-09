7d5644ff2f04f301afe41184453a69113347c210
Ying Lu <yinglu5374@gmail.com>
2023-12-08 21:16:09 -0500

    init

---
+++ app/actions/index.tsx
@@ -0,0 +1,19 @@
+'use server'
+
+import AnimeCard, { AnimeObject } from '@/components/AnimeCard'
+
+export const fetchAnime = async (page: number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  )
+
+  const data = await response.json()
+
+  return data.map((item: AnimeObject, index: number) => (
+    <AnimeCard
+      key={item.id}
+      anime={item}
+      index={index}
+    />
+  ))
+}
